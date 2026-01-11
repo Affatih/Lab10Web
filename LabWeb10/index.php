b487870fde85d81aa54f70258f35215b1f3f61c0
Affatih <eftehaplusenamdua@gmail.com>
2026-01-11 03:58:28 -0500

    Delete LabWeb10 directory

--- LabWeb10/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-// index.php: halaman awal, redirect ke dashboard
-header('Location: /lab10_php_oop/views/dashboard.php');
-exit;
--- LabWeb10/modules/auth/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-// modules/auth/logout.php - proses logout
-session_start();
-session_unset();
-session_destroy();
-
-header('Location: login.php');
-exit;
